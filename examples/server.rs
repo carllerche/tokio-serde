e9cd466749e146af8a540b0b868098c24c56d133
Logan Magee <mageelog@gmail.com>
2020-12-23 20:36:28 -0900

    Bump tokio to 1.0 (#21)

--- examples/server.rs
+++ examples/server.rs
@@ -7,11 +7,13 @@ use tokio_util::codec::{FramedRead, LengthDelimitedCodec};
 #[tokio::main]
 pub async fn main() {
     // Bind a server socket
-    let mut listener = TcpListener::bind("127.0.0.1:17653").await.unwrap();
+    let listener = TcpListener::bind("127.0.0.1:17653").await.unwrap();
 
     println!("listening on {:?}", listener.local_addr());
 
-    while let Some(socket) = listener.try_next().await.unwrap() {
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+
         // Delimit frames using a length header
         let length_delimited = FramedRead::new(socket, LengthDelimitedCodec::new());
 
